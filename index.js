8b101f9183d6c9eda43490c4227178819acc3526
unknown <feri.18421026@student.ubl.ac.id>
2024-01-16 11:35:08 +0700

    first commit

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 import userRouter from "./src/routes/user-router.js";
+
 app.use('/user', userRouter);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
